9724ff76fe62b888ce5dea5459928a8337c739a3
Mehmet <memetalce5@gmail.com>
2012-11-10 20:49:47 +0100

    TekNaProgramata2

--- Stefan/src/RegistrationForm.java
+++ Stefan/src/RegistrationForm.java
@@ -1,3 +1,4 @@
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
@@ -65,6 +66,55 @@ final JComboBox combo=new JComboBox(titleList);
 combo.setSelectedIndex(2);
 combo.setBounds(75, 135, 150, 20);
 panel.add(combo);
+
+
+final JLabel Obrazovanie=new JLabel("Obrazovanie");
+Obrazovanie.setBounds(10,160,100,50);
+panel.add(Obrazovanie);
+
+
+final String[] knowlageList={"Osnovno","Sredno","Visoko"};
+final JRadioButton buton=new JRadioButton(knowlageList[0]);
+final JRadioButton buton1=new JRadioButton(knowlageList[1]);
+final JRadioButton buton2=new JRadioButton(knowlageList[2]);
+
+buton.setActionCommand(knowlageList[0]);
+buton1.setActionCommand(knowlageList[1]);
+buton2.setActionCommand(knowlageList[2]);
+
+buton.setSelected(true);
+
+buton.setBounds(80, 210, 250, 15);
+buton1.setBounds(80, 240, 250, 15);
+buton2.setBounds(80, 270, 250, 15);
+
+
+panel.add(buton);
+panel.add(buton1);
+panel.add(buton2);
+
+ButtonGroup grupa=new ButtonGroup();
+grupa.add(buton);
+grupa.add(buton1);
+grupa.add(buton2);
+
+final JLabel iskustvo=new JLabel("Iskustvo");
+iskustvo.setBounds(10,300,100,50);
+panel.add(iskustvo);
+
+final JTextField iskustvo1=new JTextField();
+iskustvo1.setBounds(75,315,150,20);
+panel.add(iskustvo1);
+
+final JLabel jazik=new JLabel("Telefon");
+jazik.setBounds(10,340,100,50);
+panel.add(jazik);
+
+final JTextField jazik1=new JTextField();
+jazik1.setBounds(75,355,150,20);
+panel.add(jazik1);
+
+
 
 
 
